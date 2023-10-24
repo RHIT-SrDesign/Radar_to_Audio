7263b28481eab55d8428be59774f15a9003a784e
vanderjt <vanderjt@rose-hulman.edu>
2023-10-23 23:49:41 -0400

    Commiting all radar visualization code I've done

--- SynthRad21/midas_tools.py
+++ SynthRad21/midas_tools.py
@@ -9,24 +9,25 @@ from bluefile.bluefile import readheader,unpack_data_from_stream,_rep_tran
 
 class MidasFile(object):
     def __init__(self,file_path):
-        self.file_path = r"C:\Users\fennelj1\Desktop\radarAudio\SynthRad21\data\iq\angry_alpaca_sas.tmp"
+        self.file_path = file_path
         self._read_header()
         # 'size' is the number of elements in the file
         # 'bpe' is the number of bytes per element
-        self.n_bytes_in_file = self.header['size'] * self.header['bpe']
-        self.endian = _rep_tran[self.header['data_rep']]
-        self.sample_rate = round(1/self.header['xdelta'])
-        self.data_duration = self.header['size']*self.header['xdelta']
+        self.n_bytes_in_file = self.header['size'] * self.header['bpe'] #This value is read in from the header block as per MIDAS blue file standard
+        self.endian = _rep_tran[self.header['data_rep']] #This value is read in from the header block as per MIDAS blue file standards
+        self.sample_rate = round(1/self.header['xdelta']) #This value reads in the interval between consecutive samples and finds the reciporical likely measured in Hz
+        self.data_duration = self.header['size']*self.header['xdelta'] #Reads in the number of data elements and multiplies by the interval between consecutive samples
+        self.n_elements = self.header['size']
         try:
-            self.fp = open(self.file_path,'rb')
+            self.fp = open(self.file_path,'rb') #Opens the file in read mode with binary I/O
         except Exception as e:
-            print('Exception caught in MidasFile.__init__: {}'.format(e))
+            print('Exception caught in MidasFile.__init__: {}'.format(e)) #if the file doesn't open prints an error and gracefully exits
             sys.exit(1)
 
-    def _read_header(self):
+    def _read_header(self): #function read header uses the read header helper function at the location of the file
         self.header = readheader(self.file_path)
 
-    def seek_to_time(self,t,whence=0):
+    def seek_to_time(self,t,whence=0): #jumps to element at this time event and starts from there
         ''' 
         WARNING:
         The try-except block will execute silently if t > tmax (i.e., we ask to
@@ -42,44 +43,44 @@ class MidasFile(object):
         whence=2: end of file
         '''
 
-        n_bytes_to_seek = int(self.seconds_to_elements(t) * self.header['bpe'])
-        if whence == 0:
-            # advance n_bytes_to_seek by header_size
-            n_bytes_to_seek += self.header['data_start']
-            n_bytes_to_seek = int(n_bytes_to_seek)
+        n_bytes_to_seek = int(self.seconds_to_elements(t) * self.header['bpe']) #takes the time input, runs seconds to elements function, and multiplies by the bytes per element
+        if whence == 0: #if seeking from the beggining of file
+           
+            n_bytes_to_seek += self.header['data_start'] #start all seek requests at the base location of where the data starts
+            n_bytes_to_seek = int(n_bytes_to_seek) #cast the number of bytes as an integer
         try:
-            self.fp.seek(n_bytes_to_seek,whence)
+            self.fp.seek(n_bytes_to_seek,whence) #run the seek command to the byte in the file
         except Exception as e:
-            print('Exception caught in MidasFile.seek_to_time: {}'.format(e))
+            print('Exception caught in MidasFile.seek_to_time: {}'.format(e)) #catch error, fail gracefully
 
-    def tell_time(self):
+    def tell_time(self): #this function returns the time of the cursor in the file 
         try:
-            t = self.fp.tell()/self.header['bpe']/self.sample_rate
+            t = self.fp.tell()/self.header['bpe']/self.sample_rate #sets time equal to the current curosr location(integer index?), divided by the bytes per element, divided by the samples per second
         except Exception as e:
-            print('Exception caught in MidasFile.tell_time: {}'.format(e))
+            print('Exception caught in MidasFile.tell_time: {}'.format(e)) #If this doesn't work handles error greacefully
             sys.exit(1)
         return t
 
-    def seconds_to_elements(self,t):
-        return int(t * self.sample_rate)
+    def seconds_to_elements(self,t): #converts current time to element of the data
+        return int(t * self.sample_rate) #multiplies time by sample rate to get element number and casts as int
 
-    def read_at_time(self,t,delta_t,reset_time=False):
+    def read_at_time(self,t,delta_t,reset_time=False): 
+        if reset_time: #if function takes in reset time as true
+            t0 = self.tell_time() #Reads the current time
+        n_elements = self.seconds_to_elements(delta_t) #sets n elements equal to seconds to elements of the time between samples
+        self.seek_to_time(t) #jump to the requested time in the function input
+        data = next(self.read(n_elements)) #sets the variable data to the result of the next function, of the total number of elements
         if reset_time:
-            t0 = self.tell_time()
-        n_elements = self.seconds_to_elements(delta_t)
-        self.seek_to_time(t)
-        data = next(self.read(n_elements))
-        if reset_time:
-            self.seek_to_time(t0)
+            self.seek_to_time(t0) #sets time all the way back to 0
         return data
         
 
-    def read(self,n_elements,n_overlap=0):
+    def read(self,n_elements,n_overlap=0): #Takes in the number of elemebts
         '''
         Generator to read from the file.
         n_elements and n_overlap are specified in terms of elements in the MIDAS file.
         '''
-        n_elements = int(n_elements)
+        n_elements = int(n_elements) #casts number of elements to read as an integer
         while self.fp.tell() < self.n_bytes_in_file:
             yield self._read_stream(n_elements,n_overlap)
                 
@@ -91,46 +92,58 @@ class MidasFile(object):
         except Exception as e:
             raise(e)
         data = np.array(data)
-        # data = data[:,0] + 1j*data[:,1]
+        mag = data
+        # mag = np.abs(data)
         if n_overlap > 0:
             self.fp.seek(-n_bytes_to_seek,1) # move pointer back according to the overlap amoutn
-        return np.array(data)
+        # return np.array(data)
+        return mag
 
 
 
 if __name__=='__main__':
     # example usage
-    file_path = 'file.tmp'
+    file_path = r"C:\Users\vanderjt\Desktop\radarAudio\givenScripts\data\iq\angry_bear_sas.tmp"
     print('Running tests on '+file_path+'...')
     mf = MidasFile(file_path)
-    n_elements = 1e6 # read this many elements 
-    n_overlap = int(n_elements/3) # overlap by a non-integer multiple just to check that things appear to be working
+    n_elements = mf.n_elements # read this many elements 
+    n_overlap = int(n_elements/2) # overlap by a non-integer multiple just to check that things appear to be working
     n_fft = 1024*2
-    window = sig.chebwin(n_fft,100,sym=False) # any window will probably do
-    
-    # # try reading an arbitrary window
-    # d = mf.read_at_time(0.5,1e-3)
-    # f,t,s = sig.stft(np.real(d),mf.sample_rate,window,nperseg=len(window),boundary=None)
-    # s_disp = 20*np.log10(np.abs(s)/np.max(np.abs(s))) # make the spectrum easier to read
-    # plt.figure()
-    # plt.pcolormesh(t,f,s_disp)
-    # plt.clim([-60, 0]) # set dynamic range
-    # plt.colorbar()
-    # plt.show()
-
+    window = sig.chebwin(n_fft,100,sym=False) # any window will probably do cheb window with 100db range, not rly sure why tho :/
 
     # reset the file
     mf.seek_to_time(0)
-    print(mf._read_stream(n_elements,n_overlap=n_overlap))
-    # loop throgh and read the whole thing
-    # for d in mf.read(n_elements,n_overlap=n_overlap):
-    #     f,t,s = sig.stft(np.real(d),mf.sample_rate,window,nperseg=len(window),boundary=None)
-    #     s_disp = 20*np.log10(np.abs(s)/np.max(np.abs(s))) # make the spectrum easier to read
-    #     print(f,t,s)
-    #     print(s_disp)
-    #     # plt.figure()
-    #     # plt.clim([-60, 0]) # set dynamic range
-    #     # plt.colorbar()
-    #     # plt.show() # close the plot to continue
-
-# test test test
+    npData = mf._read_stream(n_elements,n_overlap=n_overlap)
+    # npData = mf.read(n_elements,n_overlap=0)
+    print("data prepd")
+    index = np.array(range(mf.n_elements))
+    elements = index
+    index = index/mf.sample_rate    
+    nfft = int(pow(2, np.ceil(np.log(len(elements))/np.log(2))))
+    print("\n")
+    
+    #print(nfft)
+    N = (len(elements))
+    nfft = nfft // 16
+    fftData=np.fft.fft(npData)
+    fftFreq = np.fft.fftfreq(N,1/mf.sample_rate)
+    fftData = fftData*(1/mf.sample_rate)
+    cf = "?"
+    txt = "Frequency centered around {center}"
+    txt=txt.format(center=cf)
+    print("computing amp")
+    amplitude = (1/N) * np.abs(fftData)
+    amplitude = 20*np.log10(amplitude)
+    
+    ax1 = plt.subplot(3,1,1)
+    ax1.plot(fftFreq,amplitude)
+    plt.xlabel(txt)
+    plt.ylabel("Amplitude")
+    plt.subplot(3,1,2)
+    Pxx, freqs, bins, im = plt.specgram(npData,nfft,mf.sample_rate ,noverlap = nfft//2)   
+    plt.grid()
+    plt.subplot(3,1,3)
+    x = index[0::10]
+    y = (np.abs(npData))[0::10]
+    plt.scatter(x,y)
+    plt.show()  
