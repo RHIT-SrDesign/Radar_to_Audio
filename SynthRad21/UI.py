49683ed2b8199e5304a097b59a261efc8af916d0
vanderjt <vanderjt@rose-hulman.edu>
2024-01-17 12:10:44 -0500

    Cleaning up and commiting local

--- SynthRad21/UI.py
+++ SynthRad21/UI.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from io import BytesIO
 
-sg.theme('DarkTeal6')
+sg.theme('DarkAmber')
 
 
 file_path = None
@@ -25,7 +25,7 @@ PWA =[('\u2B24'+' No PW Agility', 'red'), ('\u2B24'+' PW Agile', 'green')]
 PWState = 0
 
 image_col = [
-    [sg.Text(text=CFA[CFState][0], text_color=CFA[CFState][1], key='INDICATOR1'), sg.Text(text=PRIA[PRIState][0], text_color=CFA[CFState][1], key='INDICATOR2'),sg.Text(text=PWA[PWState][0], text_color=CFA[CFState][1], key='INDICATOR3')],
+    [sg.Text(text=CFA[CFState][0], text_color=CFA[CFState][1], key='INDICATOR1'), sg.Text(text=PRIA[PRIState][0], text_color=CFA[PRIState][1], key='INDICATOR2'),sg.Text(text=PWA[PWState][0], text_color=CFA[PWState][1], key='INDICATOR3')],
     [sg.Image('SynthRad21\Angry_bear.GIF', key = 'display')],
 
     
